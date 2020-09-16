b8110f432023892db90122d9f314b59b35aca55d
Kayla Newlon <kbnewlon@hotmail.com>
2020-09-15 21:33:42 -0700

    populated ten questions and answers

---
+++ questions.js
@@ -0,0 +1,127 @@
+const quizQuestions = [
+
+{
+    question: "How does Harry manage to breathe underwater during the second task of the Triwizard Tournament?",
+    answers: {
+        a:"He transfigures into a shark",
+        b:"He kisses a mermaid   ",
+        c:"He eats gillyweed",
+        d:"He performs a bubble-head charm",
+    }
+    correctAnswer: "c"
+};
+
+
+
+{
+    question: "What is the name of Fred and George’s joke shop?",
+    answers: {
+        a:"Weasley Joke Emporium",
+        b:"Weasleys’ Wizard Wheezes",
+        c:"Fred & George’s Wonder Emporium",
+        d:"Zonko’s Joke Shop",
+    }
+    correctAnswer: "b"
+};
+
+
+{
+    question: "Which of these is NOT one of the Unforgivable Curses?",
+    answers: {
+        a:"Cruciatus Curse",
+        b:"Imperius Curse",
+        c:"Sectumsempra",
+        d:"Avada Kedavra",
+    }
+    correctAnswer: "c"
+};
+
+{
+    question: "Who played Lord Voldemort in the movies?",
+    answers: {
+        a:"Jeremy Irons",
+        b:"Tom Hiddleston",
+        c:"Gary Oldman",
+        d:"Ralph Fiennes",
+    }
+    correctAnswer: "d"
+};
+
+
+
+{
+    question: "Who guards the entrance to the Gryffindor common room?",
+    answers: {
+        a:"The Grey Lady",
+        b:"The Fat Friar",
+        c:"The Bloody Baron",
+        d:"The Fat Lady",
+    }
+    correctAnswer: "d"
+};
+
+
+{
+    question: "Who is NOT a member of the Order of the Phoenix?",
+    answers: {
+        a:"Cornelius Fudge",
+        b:"Mad-eye Moody",
+        c:"Professor Snape",
+        d:"Remus Lupin",
+    }
+    correctAnswer: "a"
+};
+
+
+
+{
+    question: "A wizard who cannot do magic is known as a:?",
+    answers: {
+        a:"Bleaker",
+        b:"Squib",
+        c:"Duddle",
+        d:"Wizont",
+    }
+    correctAnswer: "b"
+};
+
+
+
+{
+    question: "What does the spell “Obliviate” do?",
+    answers: {
+        a:"Destroys objects",
+        b:"Sends someone to the nether realm",
+        c:"Removes parts of someone’s memory",
+        d:"Makes objects invisible",
+    }
+    correctAnswer: "c"
+};
+
+{
+    question: "Where does Hermione brew her first batch of Polyjuice Potion?",
+    answers: {
+        a:"Moaning Myrtle’s Bathroom",
+        b:"The Hogwarts Kitchen",
+        c:"The Room of Requirement",
+        d:"The Gryffindor Common Room",
+    }
+    correctAnswer: "a"
+};
+
+
+
+{
+    question: "What does one say to close the Marauder’s Map and make it blank again?,
+    answers: {
+        a:"Mischief Managed",
+        b:"Nothing to See Here",
+        c:"All Done",
+        d:"Hello Professor",
+    }
+    correctAnswer: "a"
+};
+
+
+
+];
